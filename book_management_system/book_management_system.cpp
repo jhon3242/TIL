53895cd72fea3271d353f7c19e26728359d54b3b
jhon3242 <78288539+jhon3242@users.noreply.github.com>
2021-07-15 12:45:33 +0900

    this is test

--- book_management_system/book_management_system.cpp
+++ book_management_system/book_management_system.cpp
@@ -15,7 +15,7 @@ struct books{
 int main(){
     struct books list;
     int total_book_num=0, user_choice;
-
+    printf("test");
     printf("-------------------------------\n");
     printf("안녕하세요 도서 관리 프로그램입니다.\n\n");
     printf("1. 책 추가하기 \n");
