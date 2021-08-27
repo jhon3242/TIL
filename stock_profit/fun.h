ef690c17dea5b542c4ed22327c5d77c0cde1464d
jhon3242 <78288539+jhon3242@users.noreply.github.com>
2021-08-27 10:23:25 +0900

    무한 매수 수익률 확인 프로그램 vr.8
    선택지 추가.
    이전에는 전체 날마다의 수익률이나 특정 날의 매수,매도 현황을 볼 수 있었는데 이제는 사용자가 티커를 입력한 뒤에 선택을 하여 특정한 날의 수익률만 볼 것인지 전체 수익률을 볼 것인지 선택하여 확인할 수 있다.

--- stock_profit/fun.h
+++ stock_profit/fun.h
@@ -1,3 +1,9 @@
+#ifndef FUN_H
+#define FUN_H
+
 void buy( int box, float prise);
 void sell(float prise);
 int program(char *start_date, FILE *fp, int start_money);
+int program_each(char *start_date, FILE *fp, int start_money);
+#endif
+
