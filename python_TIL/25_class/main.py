e482d4664affbd33213f655645b7e78a2a0b095c
jhon3242 <rldotnr2@gmail.com>
2021-12-06 23:01:49 +0900

    TIL | 클래스

--- python_TIL/25_class/main.py
+++ python_TIL/25_class/main.py
@@ -8,10 +8,21 @@ james.greeting()
 b = list(range(10))
 b.append(20)
 print(b)
-"""
+
 def factorial(n):
     if not isinstance(n, int) or n < 0:    # n이 정수가 아니거나 음수이면 함수를 끝냄
         return None
     if n == 1:
         return 1
     return n * factorial(n - 1)
+
+
+class Person:
+	def __init__(self):
+		self.hello ='안녕하세요'
+	def greeting(self):
+		print(self.hello)
+
+james = Person()
+james.greeting()
+"""
