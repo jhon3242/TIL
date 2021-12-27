ffe6e08d75649da950ca0d1821be20989f36251a
jhon3242 <rldotnr2@gmail.com>
2021-12-27 23:28:14 +0900

    TIL | 두 점 사이의 거리 이전에 했어야 했던 파트이지만 까먹고 그냥 지나쳤었다.

---
+++ python_TIL/30_length/main.py
@@ -0,0 +1,14 @@
+from math import sqrt
+class Point2D:
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+
+p1 = Point2D(x = 30, y = 20)
+p2 = Point2D(x = 60, y = 50)
+
+a = p2.x - p1.x
+b = p2.y - p1.y
+
+c = sqrt(a*a + b*b)
+print(c)
