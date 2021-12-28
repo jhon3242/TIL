c09add7c0067ad823f1d71f687c893e6c52b8ce5
jhon3242 <rldotnr2@gmail.com>
2021-12-28 23:34:19 +0900

    TIL | 두 점 사이의 거리 collcetions

--- python_TIL/30_length/main.py
+++ python_TIL/30_length/main.py
@@ -1,3 +1,4 @@
+"""
 from math import sqrt
 class Point2D:
 	def __init__(self, x, y):
@@ -11,4 +12,17 @@ a = p2.x - p1.x
 b = p2.y - p1.y
 
 c = sqrt(a*a + b*b)
+print(c)
+ """
+
+from math import sqrt
+import collections
+
+Point2D = collections.namedtuple('Point2D', ['x', 'y'])
+p1 = Point2D(x=30, y=20)
+p2 = Point2D(x=60, y=50)
+
+a = p1.x - p2.x
+b = p1.y - p2.y
+c = sqrt(a**2 + b**2)
 print(c)
