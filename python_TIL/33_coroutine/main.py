b5e44f6989fe2b139d5b82449383bd294ce8d2f5
jhon3242 <rldotnr2@gmail.com>
2022-01-05 23:56:41 +0900

    TIL | 코루틴 진행중

--- python_TIL/33_coroutine/main.py
+++ python_TIL/33_coroutine/main.py
@@ -1,3 +1,4 @@
+"""
 def number_coroutine():
 	while (True):
 		x = (yield)
@@ -9,3 +10,52 @@ next(co)
 co.send(1)
 co.send(2)
 co.send(3)
+
+
+
+
+def sum_coroutine():
+	total = 0
+	while (True):
+		x = (yield total)
+		total += x
+
+co = sum_coroutine()
+next(co)
+print(co.send(1))
+print(co.send(2))
+print(co.send(3))
+
+
+
+def number_coroutine():
+    while True:
+        x = (yield)
+        print(x, end=' ')
+
+co = number_coroutine()
+next(co)
+
+for i in range(20):
+	co.send(i)
+
+co.close()
+"""
+
+
+def number_coroutine():
+	try:
+		while True:
+			x = (yield)
+			print(x, end=' ')
+	except GeneratorExit:
+		print()
+		print("코루틴 종료")
+
+co = number_coroutine()
+next(co)
+
+for i in range(20):
+	co.send(i)
+
+co.close()
