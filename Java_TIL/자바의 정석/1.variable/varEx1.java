d347ea2fc38fba55a937b2b05ecfce99d31d0930
jhon3242 <rldotnr2@gmail.com>
2022-03-31 20:57:05 +0900

    자바의 정석 공부 시작

---
+++ Java_TIL/자바의 정석/1.variable/varEx1.java
@@ -0,0 +1,22 @@
+package _ch2;
+
+public class VarEx03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		int score = 100;
+//		score = 200;
+		boolean power = true;
+		final int score = 100;
+		int i = 100; // 10진수
+		int oct = 0100; // 8진수
+		int hex = 0x100; // 16 진수 
+		float f_i = 3.14f;
+//		double d_j = le3;
+		double d_i = 123L;
+		System.out.println(score);		
+	}
+}
+   
+
+
---
+++ Java_TIL/자바의 정석/1.variable/varEx2.java
@@ -0,0 +1,12 @@
+package _ch2;
+
+public class VarEx4 {
+
+	public static void main(String[] args) {
+		String s1 = "AB";
+		String s2 = new String("BA");
+		s1 = "";
+		System.out.println(s1 + 7);
+	}
+}
+   
