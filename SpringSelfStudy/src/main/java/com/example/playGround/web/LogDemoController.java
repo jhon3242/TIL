7e9559a5dbbfac067052a30ead92b4b0d3d2bbf1
jhon3242 <rldotnr2@gmail.com>
2022-05-17 15:50:12 +0900

    add | 웹 스코프 에러 해결 방법 1 : Provider ObjectProvider 를 통해 스프링 컨테이너의 요청을 지연하고 HTTP 요청이 오는 시점에는 MyLogger 가 존재하기 때문에 해결할 수 있다.

--- SpringSelfStudy/src/main/java/com/example/playGround/web/LogDemoController.java
+++ SpringSelfStudy/src/main/java/com/example/playGround/web/LogDemoController.java
@@ -3,6 +3,7 @@ package com.example.playGround.web;
 
 import com.example.playGround.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,12 +15,13 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
 	private final LogDemoService logDemoService;
-	private final MyLogger myLogger;
+	private final ObjectProvider<MyLogger> myLoggerProvider;
 
 	@GetMapping("log-demo")
 	@ResponseBody
 	public String logDemo(HttpServletRequest request){
 		String rURL = request.getRequestURL().toString();
+		MyLogger myLogger = myLoggerProvider.getObject();
 		myLogger.setRequestUrl(rURL);
 		myLogger.log("controll test");
 		logDemoService.logic("testId");
