da46c53a50c01cd1723e16f43ae72e873c588418
jhon3242 <rldotnr2@gmail.com>
2022-05-17 15:52:55 +0900

    add | 웹 스코프 에러 해결 방법 2 : 프록시 프록시를 사용하여 껍데기 MyLogger 를 넣어준다음에 실제 MyLogger를 사용할 시점에 실제 MyLogger 를 찾는 로직이 실행되도록 하는 방법이다. 마치 싱글톤을 사용하듯이 사용할 수 있지만 이런 특별한 scope 는 무분별하게 사용하지 않도록 주의해야한다.

--- SpringSelfStudy/src/main/java/com/example/playGround/web/LogDemoService.java
+++ SpringSelfStudy/src/main/java/com/example/playGround/web/LogDemoService.java
@@ -3,16 +3,14 @@ package com.example.playGround.web;
 
 import com.example.playGround.common.MyLogger;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
-	private final ObjectProvider<MyLogger> myLoggerProvider;
+	private final MyLogger myLogger;
 
 	public void logic(String id){
-		MyLogger myLogger = myLoggerProvider.getObject();
 		myLogger.log("service id = " + id);
 	}
 }
