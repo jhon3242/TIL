48f72173b4466fcf5afb59360eac213ccb92c86c
jhon3242 <rldotnr2@gmail.com>
2022-05-17 14:16:05 +0900

    add | 콜백 메서드 지원 방법 1 : 인터페이스를 활용한 방법 InitializingBean, DisposableBean 외 같은 초기화, 소멸 인터페이스를 활용하는 방법이다. 단점으로는 초기화, 종료 메서드 이름을 수정할 수 없고 스프링 의존적이라 외부 라이브러리에서는 사용할 수 없다는 단점 등이 있다.

---
+++ SpringSelfStudy/src/test/java/com/example/playGround/lifecycle/BeanLifecycleTest.java
@@ -0,0 +1,31 @@
+package com.example.playGround.lifecycle;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifecycleTest {
+
+	@Test
+	@DisplayName("빈 생명주기 확인")
+	void lifeCycleTest(){
+		ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+		NetworkClient client = ac.getBean(NetworkClient.class);
+		ac.close();
+	}
+
+	@Configuration
+	static class LifeCycleConfig{
+
+		@Bean
+		public NetworkClient networkClient(){
+			NetworkClient networkClient = new NetworkClient();
+			networkClient.setUrl("https://hello-spring.dev");
+			return networkClient;
+		}
+	}
+}
---
+++ SpringSelfStudy/src/test/java/com/example/playGround/lifecycle/NetworkClient.java
@@ -0,0 +1,44 @@
+package com.example.playGround.lifecycle;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+import javax.naming.Context;
+import javax.naming.NamingException;
+import javax.naming.spi.InitialContextFactory;
+import java.util.Hashtable;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
+	private String url;
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public NetworkClient() {
+		System.out.println("생성자 호출 url = " + url);
+	}
+
+	public void connect(){
+		System.out.println("connect : " + url);
+	}
+	
+	public void call(String message){
+		System.out.println("call : " + url + " message = " + message);
+	}
+
+	public void disconnect(){
+		System.out.println("close : " + url);
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		connect();
+		call("초기화 연결 메시지");
+	}
+
+	@Override
+	public void destroy() throws Exception {
+		disconnect();
+	}
+}
